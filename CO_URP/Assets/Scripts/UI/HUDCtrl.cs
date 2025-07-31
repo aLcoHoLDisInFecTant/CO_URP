71d8f455c9eeb803edaaf99bb4b9d559f9424a9c
xiangyuwang <Xiangyu.Wang21@student.xjtlu.edu.cn>
2025-08-01 00:54:28 +0800

    流程测试ok

--- CO_URP/Assets/Scripts/UI/HUDCtrl.cs
+++ CO_URP/Assets/Scripts/UI/HUDCtrl.cs
@@ -7,15 +7,27 @@ using TMPro; // 引用TextMeshPro的命名空间
 /// </summary>
 public class HUDCtrl : MonoBehaviour
 {
+    public int starCount;
+
     // 在Unity编辑器中，将playerHUD的子对象（用于显示金币数量的TMP Text）拖拽到这个字段上
     [SerializeField]
     private TMP_InputField coinCountText;
     [SerializeField]
     private TMP_InputField ScoreText;
+    [SerializeField]
+    private GameObject starIcon;
+    private GameObject starIcon_2;
+    private GameObject starIcon_3;
+    private int starsCount = 0;
 
     void Start()
     {
+        EventManager.StartListening("LightStar", LightStar);
+        EventManager.StartListening("ResultMedium", BoostResult);
         // 确保coinCountText已经被赋值，如果没有，在控制台输出错误提示
+        starIcon.SetActive(false);
+        starIcon_2.SetActive(false);
+        starIcon_3.SetActive(false);
         if (coinCountText == null)
         {
             Debug.LogError("HUDCtrl脚本上的Coin Count Text没有被指定！请在Inspector面板中拖拽一个TextMeshProUGUI组件。", this.gameObject);
@@ -36,5 +48,30 @@ public class HUDCtrl : MonoBehaviour
             ScoreText.text = currentScore.ToString();
 
         }
+    }
+
+    void LightStar(object id) 
+    {
+        switch (id) 
+        {
+            case 0:
+                starIcon.SetActive(true);
+                starCount++;
+                break;
+            case 1: 
+                starIcon_2.SetActive(true);
+                starCount++;
+                break;
+            case 2:
+                starIcon_3.SetActive(true);
+                starCount++;
+                break;
+        }
+    }
+
+    void BoostResult(object data) 
+    {
+        EventManager.TriggerEvent("settleResult", starCount);
+    
     }
 }
--- CO_URP/Assets/Scripts/UI/ResultDisplayManager.cs
+++ CO_URP/Assets/Scripts/UI/ResultDisplayManager.cs
@@ -2,21 +2,33 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using System;
+using TMPro;
+using Unity.VisualScripting;
 
 public class ResultDisplayManager : MonoBehaviour
 {
     [Header("UI Elements")]
-    public Text coinText;
-    public Text scoreText;
-    public Text timeText;
-    public Text operationCountText;
-    public Text operationDurationText;
+    public GameObject result;
+    public TMP_InputField coinText;
+    public TMP_InputField scoreText;
+    public TMP_InputField timeText;
+    public TMP_InputField operationCountText;
+    public TMP_InputField operationDurationText;
 
     [Header("External References")]
     public PlayerOperationLogger operationLogger;
 
+    [Header("Star")]
+    public GameObject star01;
+    public GameObject star02;
+    public GameObject star03;
+    public GameObject fadeStar01;
+    public GameObject fadeStar02;
+    public GameObject fadeStar03;
+
     void Start()
     {
+        EventManager.StartListening("settleResult", ShowResults);
         if (operationLogger == null)
             operationLogger = FindObjectOfType<PlayerOperationLogger>();
     }
@@ -24,8 +36,10 @@ public class ResultDisplayManager : MonoBehaviour
     /// <summary>
     /// 外部调用该方法以显示结算信息
     /// </summary>
-    public void ShowResults()
+    public void ShowResults(object data)
     {
+        result.SetActive(true);
+        Time.timeScale = 0f;
         // 1. Coin & Score
         int coin = ScoreHUDBridge.Instance?.CoinCount ?? 0;
         int score = ScoreHUDBridge.Instance?.GetTotalScore() ?? 0;
@@ -36,7 +50,44 @@ public class ResultDisplayManager : MonoBehaviour
         string formattedTime = SceneTimer.Instance?.GetFormattedTime() ?? "00:00";
         timeText.text = $"Time: {formattedTime}";
 
-        // 3. Operation Stats
+        // 3. Star
+        switch (data)
+        {
+            case 0:
+                star01.SetActive(false);
+                star02.SetActive(false);
+                star03.SetActive(false);
+                fadeStar01.SetActive(true);
+                fadeStar02.SetActive(true);
+                fadeStar03.SetActive(true);
+                break;
+            case 1:
+                star01.SetActive(true);
+                star02.SetActive(false);
+                star03.SetActive(false);
+                fadeStar01.SetActive(false);
+                fadeStar02.SetActive(true);
+                fadeStar03.SetActive(true);
+                break;
+            case 2:
+                star01.SetActive(true);
+                star02.SetActive(true);
+                star03.SetActive(false);
+                fadeStar01.SetActive(false);
+                fadeStar02.SetActive(false);
+                fadeStar03.SetActive(true);
+                break;
+            case 3:
+                star01.SetActive(true);
+                star02.SetActive(true);
+                star03.SetActive (true);
+                fadeStar01.SetActive(false);
+                fadeStar02.SetActive(false);
+                fadeStar03.SetActive(false);
+                break; 
+        }
+
+        // 4. Operation Stats
         int opCount = 0;
         float totalDuration = 0f;
 
@@ -66,5 +117,10 @@ public class ResultDisplayManager : MonoBehaviour
 
         operationCountText.text = $"Effective Ops: {opCount}";
         operationDurationText.text = $"Op Time: {totalDuration:F2}s";
+    }
+
+    public void OnApplicationQuit()
+    {
+        Debug.Log("quit");
     }
 }
